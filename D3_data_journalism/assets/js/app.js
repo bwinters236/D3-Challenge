9db5c47501800f9f99a98b0e156cd1e07852f624
Ben Winters <bwinters236@gmail.com>
2021-04-25 03:03:09 -0400

    Comments cleaned

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,6 +1,8 @@
+// SVG dimensions
 var svgWidth = 960;
 var svgHeight = 500;
 
+// SVG margin dimensions
 var margin = {
   top: 20,
   right: 40,
@@ -10,6 +12,7 @@ var margin = {
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
+
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
 var svg = d3.select("#scatter")
@@ -21,11 +24,11 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 
-  // Import Data
+// Uses d3 to set the csv into stateData
 d3.csv("data.csv").then(function(stateData) {
 
-    // Step 1: Parse Data/Cast as numbers
-    // ==============================
+    // Parse Data/Cast as numbers
+
     stateData.forEach(function(data) {
       data.id = +data.id;
       data.state = +data.state;
@@ -34,18 +37,20 @@ d3.csv("data.csv").then(function(stateData) {
       data.obesity = +data.obesity;
     });
 
+    // Setting the x and y scale for the axis, using the min and max to zoom and center the data in the chart
     var xLinearScale = d3.scaleLinear()
-        .domain([0, d3.max(stateData, d => d.poverty)])
+        .domain([d3.min(stateData, d => d.poverty) - 1, d3.max(stateData, d => d.poverty)])
         .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-        .domain([0, d3.max(stateData, d => d.obesity)])
+        .domain([d3.min(stateData, d => d.obesity) - 1, d3.max(stateData, d => d.obesity)])
         .range([height, 0]);
 
-
+    // Setting and x and y axis to the scales made above
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
 
+    // Appending the chart with the axis
     chartGroup.append("g")
         .attr("transform", `translate(0, ${height})`)
         .call(bottomAxis);
@@ -53,6 +58,9 @@ d3.csv("data.csv").then(function(stateData) {
     chartGroup.append("g")
         .call(leftAxis);
 
+
+
+    // Appending the chart with the circle group, made from x,y dictated by poverty and obesity     
     var circleGroup = chartGroup.selectAll("circle")
         .data(stateData)
         .enter()
@@ -62,37 +70,27 @@ d3.csv("data.csv").then(function(stateData) {
         .attr("r", "15")
         .attr("class", "stateCircle")
         .attr("opacity", ".5");
+        
 
 
+
+
+        // SHOULD set text in each circle to state abbr, why does this not work?
         circleGroup.append("text")
         .text(function(d){
           return d.abbr
-        })
+        }) 
         .attr("x", d => xLinearScale(d.poverty))
         .attr("y", d => yLinearScale(d.obesity))
         .attr("class", "stateText")
         .attr("dy", +4)
         .attr("font-size", 10)
+        .attr("alignment-baseline", "central")
 
 
 
 
-    chartGroup.selectAll("div")
-      .data(stateData)
-      .enter()
-      .append("text")
-
-      .attr("x", function (d){
-          return xLinearScale(d.poverty);
-      })
-
-      .attr("y", function (d){
-          return yLinearScale(d.obesity);
-      })
-      .attr("alignment-baseline", "central")
-
-
-        // Create axes labels
+    // Create axes labels
     chartGroup.append("text")
         .attr("transform", "rotate(-90)")
         .attr("y", 0 - margin.left + 40)
