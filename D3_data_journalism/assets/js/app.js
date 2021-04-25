d35c7f44b1bb4c3c19724aa2a75c3dd71243e8b7
Ben Winters <bwinters236@gmail.com>
2021-04-24 22:42:42 -0400

    plot added

---
+++ D3_data_journalism/assets/js/app.js
@@ -0,0 +1,97 @@
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+  // Import Data
+d3.csv("data.csv").then(function(stateData) {
+
+    // Step 1: Parse Data/Cast as numbers
+    // ==============================
+    stateData.forEach(function(data) {
+      data.id = +data.id;
+      data.state = +data.state;
+      data.abbr = +data.abbr;
+      data.poverty = +data.poverty;
+      data.obesity = +data.obesity;
+    });
+
+    var xLinearScale = d3.scaleLinear()
+        .domain([20, d3.max(stateData, d => d.poverty)])
+        .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+        .domain([0, d3.max(stateData, d => d.obesity)])
+        .range([height, 0]);
+
+
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(bottomAxis);
+
+    chartGroup.append("g")
+        .call(leftAxis);
+
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(stateData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.obesity))
+        .attr("r", "15")
+        .attr("fill", "red")
+        .attr("opacity", ".5");
+
+        chartGroup.selectAll("div")
+        .data(stateData)
+        .enter()
+        .append("text")
+
+        .attr("x", function (d){
+            return xLinearScale(d.poverty);
+        })
+
+        .attr("y", function (d){
+            return yLinearScale(d.obesity);
+        })
+        .attr("alignment-baseline", "central")
+
+        // Create axes labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("axis text");
+  
+    chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+        .attr("class", "axisText")
+        .text("axis text");
+
+
+}).catch(function(error) {
+        console.log(error);
+});
